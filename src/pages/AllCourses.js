b8151cddc5608aaa4e055ec68c957a067d9f2ee7
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:28:06 +0530

    Create AllCourses.js

---
+++ src/pages/AllCourses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CourseList from '../components/CourseList';
+
+const AllCourses = () => {
+  return (
+    <div className="all-courses">
+      <h2>All Courses</h2>
+      <CourseList />
+    </div>
+  );
+};
+
+export default AllCourses;
