d553215dea82e42d83ea7b19a904e7a55764442b
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:30:06 +0530

    Create DataScience.js

---
+++ src/pages/DataScience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataScience = () => {
+  return (
+    <div>
+      <h2>Data Science</h2>
+      {/* Add content for Data Science */}
+    </div>
+  );
+};
+
+export default DataScience;
