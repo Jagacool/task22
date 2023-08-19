b6e8669d2793487e80f4484c51f0b94ecf2512a7
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:25:48 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">All</Link></li>
+        <li><Link to="/full-stack">Full Stack</Link></li>
+        <li><Link to="/data-science">Data Science</Link></li>
+        <li><Link to="/cyber-security">Cyber Security</Link></li>
+        <li><Link to="/career">Career</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
