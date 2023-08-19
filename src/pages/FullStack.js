b0fdca294f71e65a42220d2b315b83cb5664f5d1
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:29:43 +0530

    Create FullStack.js

---
+++ src/pages/FullStack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FullStack = () => {
+  return (
+    <div>
+      <h2>Full Stack Development</h2>
+      {/* Add content for Full Stack Development */}
+    </div>
+  );
+};
+
+export default FullStack;
