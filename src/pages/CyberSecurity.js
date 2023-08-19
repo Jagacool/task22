92af470a68ee3993d3c820d206a4cf5292592ff8
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:30:31 +0530

    Create CyberSecurity.js

---
+++ src/pages/CyberSecurity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CyberSecurity = () => {
+  return (
+    <div>
+      <h2>Cyber Security</h2>
+      {/* Add content for Cyber Security */}
+    </div>
+  );
+};
+
+export default CyberSecurity;
