cf8b90f29e116215605318a3fc02c23b45eba822
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 11:30:52 +0530

    Create Carrer.js

---
+++ src/pages/Carrer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Career = () => {
+  return (
+    <div>
+      <h2>Career</h2>
+      {/* Add content for Career */}
+    </div>
+  );
+};
+
+export default Career;
